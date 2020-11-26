55572fbd1217b4dc58c9391ae0b053c64f3b9a1b
spice-wasabi <spice-wasabi@users.noreply.github.com>
2020-11-26 22:43:38 +0900

    Userモデルの単体テスト_修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable,
          :validatable
 
-  validates :nickname,           presence: true
-  validates :email,              presence: true
-  validates :password,           presence: true, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?[\d])/}, length: { minimum: 6 }
-  validates :last_name,          presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
-  validates :first_name,         presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
-  validates :last_name_kana,     presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
-  validates :first_name_kana,    presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
-  validates :birthday,           presence: true
-
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+    validates :password,        format: { with: /\A(?=.*?[a-zA-Z])(?=.*?[\d])/}, length: { minimum: 6 }
+    validates :last_name,       format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+    validates :first_name,      format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+    validates :last_name_kana,  format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
+    validates :first_name_kana, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
+    validates :email
+  end
 end
