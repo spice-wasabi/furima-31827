f4fa563ffe67775bab52e0706df9aa4806952145
spice-wasabi <spice-wasabi@users.noreply.github.com>
2020-11-26 13:09:43 +0900

    ユーザー登録_バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,15 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
+        #  :validatable
+
+  validates :nickname,           presence: true
+  validates :password,           presence: true, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?[\d])/}, length: { minimum: 6 }
+  validates :last_name,          presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+  validates :first_name,         presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+  validates :last_name_kana,     presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
+  validates :first_name_kana,    presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
+  validates :birthday,           presence: true
 
 end
