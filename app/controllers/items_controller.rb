1473f5c024ebd19ed1f46da763a5165c46a1c666
spice-wasabi <spice-wasabi@users.noreply.github.com>
2020-12-03 17:44:55 +0900

    ログイン状態の商品出品者以外はURL直接入力してもindexに遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,6 +25,9 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 
   def update
