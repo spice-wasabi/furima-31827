0acedde7752bf71386b334f56c8b6470690a1f2a
spice-wasabi <spice-wasabi@users.noreply.github.com>
2020-12-06 22:37:49 +0900

    order_formバリデーション修正

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,21 +1,19 @@
 class OrderForm
   include ActiveModel::Model
   attr_accessor :postal_code, :city_id, :town, :building_name, :address, :phone_num,
-                :order_id, :user_id, :item_id
+                :order_id, :user_id, :item_id, :token
 
 # buyerモデルのバリデーション
   with_options presence: true do
-    validates :postal_code,   format: { with: /\A\d{3}[-]\d{4}\z/, message: "-ハイフンを入れてください"}
-    validates :city_id
-    validates :town
+    validates :postal_code,   format: { with: /\A\d{3}[-]\d{4}\z/ }
+    validates :city_id, numericality: { other_than: 0 }
     validates :address
+    validates :phone_num, format: { with: /\A[0-9]+\z/ }
     validates :order_id
   end
-  validates :building_name,   format: { with: /\A[a-zA-Z0-9]+\z/, message: "-ハイフンなし"}
-  validates :city_id, numericality: { other_than: 0 }
 
   def save
-    order = Order.create(user_id: user_id, item_id: item_id)
+    order = Order.create(user_id: user_id, item_id: item_id, token: token)
     Buyer.create( postal_code: postal_code, city_id: city_id, town: town, building_name: building_name,
                   address: address, phone_num: phone_num, order_id: order.id)
   end
