7938b80f165a5e3bf39301648e8bc5240c8e520c
spice-wasabi <spice-wasabi@users.noreply.github.com>
2020-12-08 13:37:20 +0900

    orログイン状態に関わらずURL直接入力でトップページに遷移/可読性修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,18 +1,11 @@
 class OrdersController < ApplicationController
-  before_action :item_params, only: [:index, :create]
+  before_action :item_params,          only: [:index, :create]
+  before_action :authenticate_user!,   only: [:index]
 
   def index
     @order_form = OrderForm.new 
-    if user_signed_in? 
-      if current_user.id == @item.user_id
-          redirect_to root_path
-        else
-          if Order.exists?(item_id: @item.id)
-            redirect_to root_path
-          end
-      end
-      else
-        redirect_to new_user_session_path
+    if Order.exists?(item_id: @item.id)
+      redirect_to root_path
     end
   end
 
